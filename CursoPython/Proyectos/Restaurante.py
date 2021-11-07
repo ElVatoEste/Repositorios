385ab12a37cf17d3fcc0f40b43048510c7b66fca
VatoDev <bigmalv@gmail.com>
2021-11-07 17:47:38 -0600

    entrar

--- CursoPython/Proyectos/Restaurante.py
+++ CursoPython/Proyectos/Restaurante.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 # testeo del while
 
 # Listas ***
@@ -19,23 +18,3 @@ while interruptor:
         interruptor = False
         break
     print('hola xd')
-=======
-# testeo del while
-
-
-interruptor = True 
-
-while interruptor:
-
-
-
-
-
-    
-    entrada = str(input('Desea seguir con el menu? y/n: '))
-    if entrada == "y":
-        interruptor = False
-        break
-    print('hola xd')
->>>>>>> 186abf19ab9f2a79d2746b5b473d7784ac388d12
-print('adios')
