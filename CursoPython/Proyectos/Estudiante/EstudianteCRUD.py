7b9a15a7aa9cd2282aede61bb7e0dca10cbc511a
VatoDev <bigmalv@gmail.com>
2021-11-08 14:19:39 -0600

    💥 NEW: Diccionario Implementado

---
+++ CursoPython/Proyectos/Estudiante/EstudianteCRUD.py
@@ -0,0 +1,11 @@
+Estudiante = {
+    'Estudiante1': {'Nom1':'Jose', 'Nom2':'Antonio', 'Apellido1':'Martinez', 'Apellido2':'Cuadras', 'Edad':26,
+    'Clases': {'Clase1': 'Fisica', 'Clase2': 'Matematica', 'Clase3': 'Ingles'}
+    },
+    'Estudiante2': {'Nom1':'Karen', 'Nom2':'Antonio', 'Apellido1':'Martinez', 'Apellido2':'Cuadras', 'Edad':26,
+    'Clases': {'Clase1': 'Fisica', 'Clase2': 'Matematica', 'Clase3': 'Ingles'}
+    },
+    'Estudiante1': {'Nom1':'Jose', 'Nom2':'Antonio', 'Apellido1':'Martinez', 'Apellido2':'Cuadras', 'Edad':26,
+    'Clases': {'Clase1': 'Fisica', 'Clase2': 'Matematica', 'Clase3': 'Ingles'}
+    },
+}
