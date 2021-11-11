3872bf1c0897833f195ed39e5157675f32d3bf60
VatoDev <bigmalv@gmail.com>
2021-11-10 21:42:50 -0600

    🔨 Refactor Code: Repasando Diccionario

--- CursoPython/dia 7-8/ejericio7.py
+++ CursoPython/dia 7-8/ejericio7.py
@@ -30,10 +30,9 @@ TurnoB = 27000
 ExtraA = 2000
 ExtraB = 3000
 
-# Establecer listas
+# Establecer diccionario
 
 Trabajadores = {}
-Salario = ()
 
 # Zona de calculos
 DiaTurnoA = TurnoA/6
@@ -44,6 +43,10 @@ MensualB = TurnoB*4
 
 
 while True:
+
+    # Dejo dentro del bucle para reiniciar mi sistema de suma
+    Salario = [] 
+
     Nomb = str(input('Ingrese el nombre del trabajador: '))
     Apellido = str(input('Ingrese el apellido del trabajador: '))
     
@@ -75,14 +78,44 @@ while True:
             print('La cantidad sobrepasa el limite maximo por semana [4]')
             Contador = int(input('Cuantos Domingos desea trabajar? [1-4]: '))
 
+        TurnoExtra = int(input(f'{NombCompleto} los domingos hace:\n1. Turno Diuno\n2. Turno Nocturno\n'))
+        while Turno != 1 and Turno != 2:
+            print('La opcion que eligio no corresponde a ninguna de la lista')
+            TurnoExtra = int(input(f'{NombCompleto} los domingos hace:\n1. Turno Diuno\n2. Turno Nocturno\n'))
+
+        if TurnoExtra == 1:
             Salario.append(ExtraA*Contador)
-            
+        else:
+            Salario.append(ExtraB*Contador)
+
     else:
         pass
     
-    TotalDomingo = sum(Salario)
-    print(f'{NombCompleto}')
+    Total = sum(Salario)
+    Trabajadores.setdefault(NombCompleto, Total)
     
-    Menu = input(str('Desea salir del sistema? y/n: '))
-    if Menu == ('y'):
+    if Turno == 1:
+        if Extra == 'y':
+            if TurnoExtra == 1:
+                print(f'A {NombCompleto} le pagaran {DiaTurnoA} diario\n, mensaulmente son {MensualA}\al hacer {Contador} domingos extra, se le agregara a su salario {ExtraA*Contador}')
+            else:
+                print(f'A {NombCompleto} le pagaran {DiaTurnoA} diario\n, mensaulmente son {MensualA}\nal hacer {Contador} domingos extra, se le agregara a su salario {ExtraB*Contador}')
+
+        print(f'A {NombCompleto} le pagaran {DiaTurnoA} diario, mensualmente seran {MensualA} al no trabajar los domingos ')
+    else:
+        if Extra == 'y':
+            if TurnoExtra == 1:
+                print(f'A {NombCompleto} le pagaran {DiaTurnoB} diario\n, mensaulmente son {MensualB}\al hacer {Contador} domingos extra, se le agregara a su salario {ExtraA*Contador}')
+            else:
+                print(f'A {NombCompleto} le pagaran {DiaTurnoB} diario\n, mensaulmente son {MensualB}\nal hacer {Contador} domingos extra, se le agregara a su salario {ExtraB*Contador}')
+
+        print(f'A {NombCompleto} le pagaran {DiaTurnoB} diario, mensualmente seran {MensualB} al no trabajar los domingos ')
+
+
+    Menu = input(str('Seguira agregando trabajadores? y/n: '))
+    if Menu == ('n'):
+
+        for x in Trabajadores:
+            print(Trabajadores)
+
         exit()
