6f4788b9e5f1ad3ff3279a0eaaf08951f5c4bee5
VatoDev <bigmalv@gmail.com>
2021-11-10 18:35:49 -0600

    👽 exposición

---
+++ CursoPython/dia 7-8/ejericio7.py
@@ -0,0 +1,20 @@
+'''
+Los empleados de una fábrica trabajan en dos turnos diurno y nocturno, se desea Calcular el
+jornal diario y el total devengado de cada uno de ellos durante una semana de trabajo de
+acuerdo con el siguiente criterio:
+
+• Tarifa diurna 18000 Córdobas
+• Tarifa nocturna 27000 Córdobas
+
+Caso de ser domingo la tarifa se incrementará en 2000 pesos diurno y 3000 nocturno
+Atributos:
+nombre: tipo cadena (debe ser nombre y apellido)
+turnoA: tipo cadena y turnoB: tipo cadena
+tarifaD y tarifaN: tipo int
+día: tipo cadena
+
+'''
+
+print('\n-------------------------------')
+print('Sistemas de tarifas laborales')
+print('-------------------------------\n')
