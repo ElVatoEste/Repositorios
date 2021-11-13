72789fc0966adf1b8cb90afcc18d3485c3425f48
VatoDev <bigmalv@gmail.com>
2021-11-12 22:00:17 -0600

    💩 Shit momento

---
+++ CursoPython/dia 7-8/ejericio2.py
@@ -0,0 +1,20 @@
+'''
+Realizar un programa que solicite una temperatura en grados Fahrenheit y convierta el
+resultado a grados Celsius.
+Para realizar el algoritmo se ha de utilizar la siguiente formula:
+Centigrados = ( 5/9 ) * ( Grados Fahrenheit – 32 )
+
+'''
+print('\n---------------------------------------------------')
+print('  Bienvenido al sistema de medidas de temperatura')
+print('---------------------------------------------------\n')
+
+
+
+
+
+while True:
+    print('Seleccione la medida a la que se desea convertir: ')
+    print('1. Grados centigrados a Fahreheit\n2. Grados Fahreheit a Centigrados')
+    
+    int(input)
