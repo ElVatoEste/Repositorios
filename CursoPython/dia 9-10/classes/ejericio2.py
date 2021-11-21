82c633789d03baa548f4af1fb190d6fcb0b978ee
VatoDev <bigmalv@gmail.com>
2021-11-21 17:05:50 -0600

    🚧 Work in Progress:  Bug fix

---
+++ CursoPython/dia 9-10/classes/ejericio2.py
@@ -0,0 +1,28 @@
+print('\nBienvenido a la agenda python\n')
+
+Contactos = []
+
+class Agenda():
+    def __init__(self):
+        Cantidad = int(input('Cuantos contactos desea agregar?: '))
+        for x in range(Cantidad):
+            Diccionario = {}
+            nom = str(input('¿Cual es el nombre de su contacto?: '))
+            Telefono = int(input('¿Cual es la edad de su contacto?: '))
+            email = str(input('¿Cual es el correo de su contacto?: '))
+            Diccionario.setdefault
+            Contactos.append
+
+
+A = Agenda()
+
+'''
+while True:
+    print('\nQue opcion desea elegir:\n#1 Suma\n#2 Resta\n#3 Multiplicación\n#4 Division')
+    Ask = int(input())
+
+    if Ask == 1:
+    if Ask == 2:
+    if Ask == 3:
+    if Ask == 4:
+'''
