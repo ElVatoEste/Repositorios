efbdd25b5ce0b4697f2f22c246125b0cca10a087
ElVatoEste <bigmalv@gmail.com>
2021-11-22 19:15:13 -0600

    ✅ Adding Tests:

---
+++ CursoPython/dia 10-11/CrearModuloPaquete/Paquete/setup.py
@@ -0,0 +1,10 @@
+from setuptools import extension, setup
+setup(
+    name= 'Paquete_Python',
+    version= '1.0.1',
+    description= ('Es un paquete practica'),
+    author= 'Vato_Dev',
+    author_email= 'https://github.com/ElVatoEste',
+    packages=['Paquete', 'Paquete.hola', 'Paquete.Adios'],
+    scripts=['']
+)
