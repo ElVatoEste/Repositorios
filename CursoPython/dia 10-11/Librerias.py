ec9cf5777ece10fd6239f08c809c7de2e14f112f
ElVatoEste <bigmalv@gmail.com>
2021-11-22 18:47:51 -0600

    💥

--- CursoPython/dia 10-11/Librerias.py
+++ CursoPython/dia 10-11/Librerias.py
@@ -1,9 +1,17 @@
 from collections import Counter
-
+'''
 Num = [1,2,3,1,2,5]
 Var = 'Python'
 
 print(Counter(Var))
 
 print(Counter(Num))
+'''
 
+Frase = 'Hola Mundo Hola Python'
+C = Counter(Frase.split())
+
+print(C.most_common())
+print(C.most_common(1))
+print(C.most_common(2))
+print(C.most_common(3))
