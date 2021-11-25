329f90a4e01494f95c3fef8af3eea09baf7cdfc9
ElVatoEste <bigmalv@gmail.com>
2021-11-24 18:48:54 -0600

    ✨

--- CursoPython/dia 12-13/TK/label.py
+++ CursoPython/dia 12-13/TK/label.py
@@ -8,5 +8,12 @@ ventana.title('Titulo')
 Lbl = Label(ventana, text='Hola mundo')
 Lbl.pack(anchor= CENTER)
 
+Lbl.config(
+    fg= 'blue',
+    bg= 'red',
+    font= ('Times', 100),
+    cursor= 'watch'
+)
+
 # Creacion de ciclo para mantener ventana abierta
 ventana.mainloop()
