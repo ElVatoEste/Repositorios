a8f64d6830e974b96a3cb58d69286e22eee4e15c
ElVatoEste <bigmalv@gmail.com>
2021-11-24 18:14:25 -0600

    👌

--- CursoPython/dia 7-8/ejericio4.py
+++ CursoPython/dia 7-8/ejericio4.py
@@ -1,4 +1,4 @@
-i = 4
+
 '''
 Un restaurante tiene la necesidad de presentar su menú de forma virtual por ende se necesita de un
 programa el cual muestre en pantalla el menú del restaurante con sus precios y el tiempo a esperar,
@@ -8,18 +8,18 @@ El desglose sería el siguiente: La opción pollo frito seguido del precio y del
 
 
 Menu = {
-    1:{'Nombre': 'Pollo Frito', 'Precio': 150, 'Tiempo': 15},
-    2:{'Nombre': 'Pizza', 'Precio': 220, 'Tiempo': 20},
-    3:{'Nombre': 'Hamburgesa', 'Precio': 120, 'Tiempo': 10}
+    'Pollo Frito':{'Nombre': 'Pollo Frito', 'Precio': 150, 'Tiempo': 15},
+    'Pizza':{'Nombre': 'Pizza', 'Precio': 220, 'Tiempo': 20},
+    'Hamburguesa':{'Nombre': 'Hamburguesa', 'Precio': 120, 'Tiempo': 10}
 }
 
 print('\n---------------------------------------------------')
 print('         Bienvenido al restaurante el jefe         ')
-print('---------------------------------------------------\n')
+print('---------------------------------------------------')
 
 while True:
 
-    Ask = int(input('Opciones del restaurante\n#1 Menu de comidas\n#2 Solicitar plato\n#3 Agregar platillo\n#4 Salir\n'))
+    Ask = int(input('\nOpciones del restaurante\n#1 Menu de comidas\n#2 Solicitar plato\n#3 Agregar platillo\n#4 Salir\n#'))
 
     if Ask == 1:
         for x in Menu:
@@ -39,8 +39,7 @@ while True:
         Ncomida = str(input('Ingrese el nombre de la comida que desea agregar: '))
         Pcomida = int(input(f'Cuanto costara el plato {Ncomida}?: '))
         Tcomida = int(input('Cuantos minutos hay que esperar?: '))
-        Menu.setdefault(i,{'Nombre':Ncomida, 'Precio':Pcomida, 'Tiempo':Tcomida})
-        i = i+1
+        Menu.setdefault(Ncomida,{'Nombre':Ncomida, 'Precio':Pcomida, 'Tiempo':Tcomida})
 
     if Ask == 4:
         print('Gracias por visitarnos, regrese pronto')
