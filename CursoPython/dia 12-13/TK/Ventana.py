11f6bff6970f0518fe9209b91eb60629a9fb75f1
ElVatoEste <bigmalv@gmail.com>
2021-11-24 18:33:17 -0600

    💥 NEW:

--- CursoPython/dia 12-13/TK/Ventana.py
+++ CursoPython/dia 12-13/TK/Ventana.py
@@ -3,5 +3,12 @@ from tkinter import *
 # CREACION DE VENTANA
 ventana = Tk()
 
+ventana.title('Clases de python')
+
+Lbl = Label(ventana, text='Hola prix').pack()
+Lbl2 = Label(ventana, text='Centro').pack(anchor= CENTER)
+Lbl3 = Label(ventana, text='Derecha').pack(anchor= NW)
+Lbl4 = Label(ventana, text='Izquierda').pack(anchor= SE)
+
 # Creacion de ciclo para mantener ventana abierta
 ventana.mainloop()
