7f7ad6fe12c6cb5366639f4abdf4d00dee70fc0d
VatoDev <bigmalv@gmail.com>
2021-11-29 19:16:32 -0600

    ⚡ General Update: Ejemplos

--- CursoPython/dia 12-13/dia 13/menu.py
+++ CursoPython/dia 12-13/dia 13/menu.py
@@ -9,9 +9,10 @@ Ventana.config(menu=menubar)
 SegundoMenu = Menu(menubar, tearoff=0)
 SegundoMenu.add_command(label="Nuevo")
 SegundoMenu.add_separator()
+SegundoMenu.add_command(label="Salir", command=Ventana.quit)
 
 menubar.add_cascade(label="Archivo", menu=SegundoMenu)
 menubar.add_cascade(label="Editar")
 menubar.add_cascade(label="Ayuda")
 
-Ventana.mainloop
+Ventana.mainloop()
