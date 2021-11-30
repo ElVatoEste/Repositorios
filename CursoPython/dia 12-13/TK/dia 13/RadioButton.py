29167967da91bfe3ece1a73ece9722246487bb16
VatoDev <bigmalv@gmail.com>
2021-11-29 18:52:21 -0600

    ⚡ General Update: Lugares

---
+++ CursoPython/dia 12-13/TK/dia 13/RadioButton.py
@@ -0,0 +1,11 @@
+from tkinter import *
+from tkinter.ttk import *
+
+
+ventana = Tk()
+
+Hombre = Radiobutton(ventana, text='Hombre', value='Hombre')
+Hombre.grid(column=0, row=0)
+
+Mujer = Radiobutton(ventana, text='Mujer', value='Mujer')
+Mujer.grid(column=1, row=0)
