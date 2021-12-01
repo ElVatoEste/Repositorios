cd0cb6a326e71a140e7056d2cc4e31d1a17bbfe7
VatoDev <bigmalv@gmail.com>
2021-11-30 21:53:19 -0600

    ⚡ General Update:  Icon change

--- CursoPython/Proyectos/ProyectoFinal/InterGrafica.py
+++ CursoPython/Proyectos/ProyectoFinal/InterGrafica.py
@@ -1,10 +1,40 @@
 from tkinter import *
 import tkinter
+from tkinter import ttk
+from tkinter import messagebox
 
+# -------------------------------------------------------
+
+Usuarios = {
+    "Admin":{"password":"abcd23"} 
+
+}
+# ----------------------------
+
+Usu = StringVar
+Contra = StringVar
+
+
+def Verificar():
+    if Usu in Usuarios:
+        if Contra == Usuarios[Usu]["password"]:
+            print("Hola xd")
+        else:
+            print("Contraseña invalida")
+
+    elif Usu not in Usuarios:
+        print("Usuario invalido")
+        
+
+# -------------------------------------------------------
+
+
+#
 ventana = Tk()
 
 
-ventana.title('The game')
+ventana.title('VatGram')
+ventana.tk.call('wm', 'iconphoto', ventana._w, tkinter.PhotoImage(file="CursoPython\Proyectos\ProyectoFinal\icon.png"))
 ventana.geometry('700x420')
 ventana.resizable(0, 0) 
 
@@ -16,7 +46,10 @@ img = PhotoImage(file="CursoPython\Proyectos\ProyectoFinal\Imagen.png")
 
 lbl_image = Label(ventana, image=img).pack()
 
-NombreUsuario = Entry(ventana, justify='center')
+# ---------------------------------------------------------------------
+# Caja de texto 
+
+NombreUsuario = Entry(ventana, justify='center', textvariable=Usu)
 NombreUsuario.pack()
 NombreUsuario.config(
     bg="white",
@@ -27,7 +60,22 @@ NombreUsuario.config(
 )
 NombreUsuario.place(x= 420, y=150)
 
-IniciarSecion = Button(ventana, text='iniciar sesión')
+ContraseñaUsuario = Entry(ventana, justify='center', textvariable=Contra)
+ContraseñaUsuario.pack()
+ContraseñaUsuario.config(
+    bg="white",
+    fg="black",
+    font= ('Times', 11),
+    cursor= 'arrow',
+    width= 20
+)
+ContraseñaUsuario.place(x= 420, y=200)
+
+# -------------------------------------------------------------
+
+
+
+IniciarSecion = Button(ventana, text='iniciar sesión', command=Verificar)
 IniciarSecion.pack()
 IniciarSecion.config(
     bg="#818181",
