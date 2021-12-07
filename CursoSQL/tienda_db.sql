141f70ee9d49c63557262f02924d43aee61754dd
ElVatoEste <bigmalv@gmail.com>
2021-12-06 19:58:06 -0600

    ⚡ General Update

--- CursoSQL/tienda_db.sql
+++ CursoSQL/tienda_db.sql
@@ -37,6 +37,9 @@ SELECT *FROM MARCA
 --------------------------------------
 INSERT INTO MARCA(CODIGO, NOMBRE) VALUES (001, 'hp')
 INSERT INTO MARCA VALUES(002, 'DELL')
+INSERT INTO MARCA VALUES(003, 'Asus')
+INSERT INTO MARCA VALUES(004, 'Lenovo')
+INSERT INTO MARCA VALUES(005, 'Mac')
 
 --------------------------------------
 -- ELIMINAR DATO EN MARCA
@@ -53,7 +56,24 @@ DROP TABLE PRODUCTO
 -- ACTUALIZAR UN DATO
 --------------------------------------
 
+UPDATE MARCA SET NOMBRE = 'HP' WHERE CODIGO = 001
 
-
+--------------------------------------
+-- CONSULTAR PRODUCTO
+--------------------------------------
 SELECT *FROM MARCA
 
+--------------------------------------
+-- INGRESAR DATO
+--------------------------------------
+
+INSERT INTO PRODUCTO VALUES (001, 'Teclado', 10, 20.5, 001)
+INSERT INTO PRODUCTO VALUES (002, 'Monitor', 10, 20.5, 002)
+INSERT INTO PRODUCTO VALUES (003, 'Raton', 10, 18.5, 003)
+INSERT INTO PRODUCTO VALUES (004, 'Camara', 10, 30.3, 004)
+INSERT INTO PRODUCTO VALUES (005, 'Audifonos', 10, 10.5, 005)
+
+-- BORRAR SEGUNDO VALOR DE LA TABLA PRODUCTO
+-- ACTUALIZAR EL TERCER VALOR DE LA TABLA MARCA
+-- CONSULTAR LA TABLA PRODUCTO
+-- CONSULTAR LA TABLA MARCA
