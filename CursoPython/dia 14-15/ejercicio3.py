a2861048ca59c4307f6cbd4e9b3a85948cade2e2
VatoDev <bigmalv@gmail.com>
2021-12-10 20:07:28 -0600

    ⚡ General Update:

--- CursoPython/dia 14-15/ejercicio3.py
+++ CursoPython/dia 14-15/ejercicio3.py
@@ -1,15 +1,48 @@
 from tkinter import *
 
+
+
 ventana1 = Tk()
-ventana1.geometry("200x200")
+ventana1.geometry("320x160")
 ventana1.title("Convetidor de grados")
+ventana1.resizable(0, 0) 
+ventana1.config(
+    bg="black",
+)
 
 def Celcius():
-    ventana1.withdraw()
+    ventana1.iconify()
     ventana2 = Toplevel()
 
-def fahrenheit():
-    ventana1.withdraw()
+def Fahrenheit():
+    ventana1.iconify()
     ventana3 = Toplevel()
+
+Texto1 = Label(ventana1, text="Bienvenido al sistema convertidor de grados\nSeleccione su opción")
+Texto1.config(
+    bg="black",
+    fg="White",
+    font=("Times", 12)
+    )
+Texto1.pack(anchor=CENTER)
+
+Boton1 = Button(ventana1, text="ºC", command=Celcius)
+Boton1.config(
+    bg="black",
+    fg="White",
+    font=("Times", 18),
+    width=3
+)
+Boton2 = Frame(ventana1, text="ºF", command=Fahrenheit)
+Boton2.config(
+    bg="black",
+    fg="White",
+    font=("Times", 18),
+    width=3
+)
+
+Boton1.place(x=110, y=60)
+Boton2.place(x=170, y=60)
+
 
 ventana1.mainloop()
