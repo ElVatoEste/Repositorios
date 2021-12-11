1a6b279291f774c30718e624c1735504429b0790
VatoDev <bigmalv@gmail.com>
2021-12-10 19:40:58 -0600

    ⚡ General Update

---
+++ CursoPython/dia 14-15/ejercicio3.py
@@ -0,0 +1,15 @@
+from tkinter import *
+
+ventana1 = Tk()
+ventana1.geometry("200x200")
+ventana1.title("Convetidor de grados")
+
+def Celcius():
+    ventana1.withdraw()
+    ventana2 = Toplevel()
+
+def fahrenheit():
+    ventana1.withdraw()
+    ventana3 = Toplevel()
+
+ventana1.mainloop()
