92ef9954eaca8cf878bc8a2c033480f458d0b9cc
VatoDev <bigmalv@gmail.com>
2021-12-13 21:21:40 -0600

    ⚡ General Update

--- CursoPython/dia 7-8/ejercicio1.py
+++ CursoPython/dia 7-8/ejercicio1.py
@@ -2,21 +2,26 @@
 Realizar un programa que solicite una temperatura en grados Fahrenheit y convierta el
 resultado a grados Celsius.
 Para realizar el algoritmo se ha de utilizar la siguiente formula:
-Centigrados = ( 5/9 ) * ( Grados Fahrenheit – 32 )
-
+Centigrados = ( 5/9 ) * ( Grados Fahrenheit - 32 )
 '''
+
+# Bienvenida
 print('\n---------------------------------------------------')
 print('  Bienvenido al sistema de medidas de temperatura')
 print('---------------------------------------------------\n')
 
+# Validador de opciones tipo si y no pero en ingles xd
 y_n = ['Y', 'N', 'n', 'y']
 
 while True:
+
+    # Lista tipo menu
     print('Seleccione la medida a la que se desea convertir: ')
     print('1. Grados centigrados a Fahreheit\n2. Grados Fahreheit a Centigrados')
     
     Opcion = int(input('Ingrese opción: '))
     
+    # Validador si la opcion no existe
     while Opcion != 1 and Opcion != 2:
             print('\n-----------------------------------------')
             print('           Caracter no valido')
@@ -33,6 +38,7 @@ while True:
         GradosC = (GradosF-32)*5/9
         print(f'\n{GradosF}ºF son {GradosC}ºC')
     
+    # Preguntar si desea salir y al mismo tiempo se valida el dato
     Salida = input('\n¿Desea salir del sistema? y/n: ')
     while Salida not in y_n:
         print('\n-----------------------------------------')
@@ -40,7 +46,10 @@ while True:
         print('-----------------------------------------')
         Salida = input('\n¿Desea salir del sistema? y/n: ')
 
+
     if Salida == 'y' or Salida == 'Y':
         print('\nMuchas gracias por usar nuestro sistema de medidad')
         print('             !! Vuelva pronto ¡¡\n')
+
+        # en este caso el usuario desea salir y se usa la funcion exit (La descubri en esa clase)
         exit()
