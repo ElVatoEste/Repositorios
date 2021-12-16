52afa31f8db6f93c95ed2b2943e5bd327788ad52
VatoDev <bigmalv@gmail.com>
2021-12-16 11:24:29 -0600

    ⚡ General Update:

--- CursoPython/dia 7-8/ejercicio3.py
+++ CursoPython/dia 7-8/ejercicio3.py
@@ -12,27 +12,37 @@ agradecer por su interés al banco
 
 '''
 
+# Una lista para definir los caracteres validos
+from tkinter.constants import CHAR
+
 
 y_n = ['Y', 'N', 'n', 'y']
 
 
+# Bienvenida al sistema
 print('\n********************************************************************')
 print('** Bienvenido al Sistema de préstamos del Banco Industrial Python **')
 print('********************************************************************\n')
 
+#Explicacion del sistema del banco
 print(f'En nuestro banco puedes solicitar prestamos para montar su negocion cobramos tan\nsolo con el 2% de interes [simple] mensualmente sobre el prestamo solicitado\nPara aplicar solo llena el siguiente formulario:')
 
+# Si desea, se le pide sus datos, si no pues se cierra el sistema
 Pedir = str(input('\nDesea aplicar para el prestamo? y/n: '))
 
+#Validacion
 while Pedir not in y_n:
     print('\n-----------------------------------------')
     print('           Caracter no valido')
     print('-----------------------------------------\n')
     Pedir = str(input('Desea aplicar para el prestamo? y/n: '))
 
+# Mensaje de salida
 if Pedir == 'n' or Pedir == 'N':
     print('\n**  Usted ha salido del sistema **')
     print('    !Que tenga un bonito dia!\n')
+
+# Peticion de datos
 else:
     print('\nIngrese su nombre completo: ')
     Nombre = str(input(''))
@@ -40,10 +50,10 @@ else:
     print('\nIngrese su numero de cedula\nEjemplo: 001-260204-1234J')
     Cedula = str(input(''))
 
-    while Cedula != 'xd':
+    while Cedula != len(16):
         print('\n                 Cedula invalida')
         print("    tienes que introducir 14 digitos validos")
-        print('** Utilize - para separar la cadena de digitoa **\n')
+        print('** Utilize - para separar la cadena de digitos **\n')
         Cedula = str(input(''))
 
     Ingresos = float(input('\nIngrese la cantidad de ingresos mensuales: '))
