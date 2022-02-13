f963886875ebf440bee5aaf16ba15a0db9a57ae1
VatoDev <bigmalv@gmail.com>
2022-02-12 18:40:11 -0600

    ✨

---
+++ CursoC++/Dia 1/HolaMundo.cpp
@@ -0,0 +1,14 @@
+// Para importar librerias se usa el #
+
+#include <iostream>
+
+using namespace std; //Le indicas que usas el c++ estandar y evitas complicarte a la hora de imprimir en pantallas
+
+int main(){
+
+    cout<<"Hola mundo";
+
+    return;0
+};
+
+
