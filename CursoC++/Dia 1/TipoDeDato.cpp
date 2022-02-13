bfa2c2ed1a36db8c5d06502652e6348d86a816a9
VatoDev <bigmalv@gmail.com>
2022-02-12 18:50:40 -0600

    ✨ Tipos de datos

---
+++ CursoC++/Dia 1/TipoDeDato.cpp
@@ -0,0 +1,24 @@
+// Esta libreria es fundamental
+#include <iostream>
+
+using namespace std; //Nos ahorramos al momento de imprimir
+
+int main(){
+
+    int numero = 15; // Indicador de variable
+
+    double Mayor = 2890.9232183; // Esto se usa igual que el flotante pero para numeros mas grandes
+
+    char Letras = 'a'; // Es para un caracter parecido al
+
+    // Los tipos de valores "int, float, str" son programacion universal
+
+
+    // el << es como un simbolo como un :
+    cout<<numero;
+
+
+    return 0;
+}
+
+
