f52e96f70a37e8ef3e803ac060a4035f97749128
VatoDev <bigmalv@gmail.com>
2022-02-12 18:54:55 -0600

    👌 Fundamental para pedir datos wow

---
+++ CursoC++/Dia 1/Pedir.y.Guardar.Datos.cpp
@@ -0,0 +1,18 @@
+// Para importar librerias se usa el #
+// Libreria fundamental
+
+#include <iostream>
+
+using namespace std; 
+int main(){
+
+    int numero;
+    cout<<"Digite un numero: ";
+
+    // > parecer ser para entrada y el < para salida :O
+    cin>>numero;
+
+    cout<<"\nEl numero es: "<<numero;
+
+    return 0;
+}
