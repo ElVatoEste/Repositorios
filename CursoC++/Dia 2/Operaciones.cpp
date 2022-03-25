091edd4855eead037c7b0ecf62a57257bafe9f30
VatoDev <bigmalv@gmail.com>
2022-03-24 18:13:36 -0600

    🚀 Deploy: Weas

---
+++ CursoC++/Dia 2/Operaciones.cpp
@@ -0,0 +1,16 @@
+#include <iostream>;
+using namespace std;
+
+
+// En c++ no funcionan la coma para difidir valores en un mismo linea, para eso se usa << :'v
+
+int main (){
+
+    cout<<"Introducir dos numeros: " << endl;
+    int n1, n2;
+
+    cin >>n1>>n2;
+
+    cout<<"La suma de " << n1 << " + " << n2 << " es: " << n1+n2 << endl;
+
+};
